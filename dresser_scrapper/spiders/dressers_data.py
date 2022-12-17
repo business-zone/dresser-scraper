25fd3b5b557279b0cf112e6e84dda62fe41ba586
FilipSzweda <filip.szweda1@gmail.com>
2022-12-17 20:42:42 +0100

    Add categories, add img urls, update data

--- dresser_scrapper/spiders/dressers_data.py
+++ dresser_scrapper/spiders/dressers_data.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 
 
 class DressersSpider(scrapy.Spider):
@@ -13,26 +14,35 @@ class DressersSpider(scrapy.Spider):
         for style in style_filter.css('span.m-filters_txt.is-customCheckbox_inp.js-checkSubmit::text'):
             style_name = style.get().lower().replace(" ", "").replace("ł", "l")
             style_url = f"https://www.agatameble.pl/meble/przechowywanie/komody/styl:{style_name}"
-            yield scrapy.Request(url=style_url, callback=self.parse_style)
+            yield scrapy.Request(url=style_url, callback=self.parse_style, meta={'category': style_name})
 
     def parse_style(self, response):
         pages_number = response.css("span.m-pagination_count::text").get().replace("z ", "")
         for page in range(1, int(pages_number) + 1):
             page_url = response.request.url + f"?page={str(page)}"
-            yield scrapy.Request(url=page_url, callback=self.parse_urls)
+            yield scrapy.Request(url=page_url, callback=self.parse_urls, meta={'category': response.meta["category"]})
 
     def parse_urls(self, response):
         for dresser in response.css("div.m-offerBox_name"):
             dresser_url = f"https://www.agatameble.pl{dresser.css('a').attrib['href']}"
-            yield scrapy.Request(url=dresser_url, callback=self.parse_dresser)
+            yield scrapy.Request(url=dresser_url, callback=self.parse_dresser,
+                                 meta={'category': response.meta["category"]})
 
     def parse_dresser(self, response):
-        name = response.css("h1.m-typo.m-typo_primary.is-full::text").get().replace('"', "").strip().strip()
-        if not name:
-            name = response.css("h1.m-typo.m-typo_primary::text").get().replace('"', "").strip().strip()
+        if name := response.css("h1.m-typo.m-typo_primary.is-full::text"):
+            name = name.get().replace('"', "").strip()
+        else:
+            name = response.css("h1.m-typo.m-typo_primary::text").get().replace('"', "").strip()
         price = response.css("div.m-priceBox_price.m-priceBox_promo::text").get().replace("-", "").replace('"',
                                                                                                            "").strip()
+        category = response.meta["category"]
+        img_urls_string = ""
+        for img_url_src in [img.attrib['src'] for img in response.css("img.undefined")]:
+            img_urls_string += f"https://www.agatameble.pl{img_url_src}, "
+        img_urls_string = re.sub(", $", "", img_urls_string)
         yield {
             "name": name,
-            "price": price
+            "price tax included": price,
+            "category": category,
+            "img url": img_urls_string
         }
